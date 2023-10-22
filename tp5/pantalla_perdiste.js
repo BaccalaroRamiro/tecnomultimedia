f6e3287d3e5b0de3670d0cfde3bc05a10e4773cc
BaccalaroRamiro <103059689+BaccalaroRamiro@users.noreply.github.com>
2023-10-22 04:28:14 -0300

    pantalla_perdiste.js

--- tp5/pantalla_perdiste.js
+++ tp5/pantalla_perdiste.js
@@ -1,9 +1,1 @@
-class PantallaPerdiste {
-  mostrar() {
-    background(0); // Fondo negro
-    textSize(32);
-    fill(255);
-    textAlign(CENTER);
-    text("¡Perdiste! Haz clic en el botón para volver a jugar.", width / 2, height / 2);
-  }
-}
+
