7dfe634681125ebcd8181479c47ea9e1ac9bb2bd
BaccalaroRamiro <103059689+BaccalaroRamiro@users.noreply.github.com>
2023-10-22 04:26:53 -0300

    Update pantalla_inicio.js

--- tp5/pantalla_inicio.js
+++ tp5/pantalla_inicio.js
@@ -1,9 +1,1 @@
-class PantallaInicio {
-  mostrar() {
-    background(0); // Fondo negro
-    textSize(32);
-    fill(255);
-    textAlign(CENTER);
-    text("¡Haz clic en el botón para comenzar!", width / 2, height / 2);
-  }
-}
+
